eebbf3f08881319ea89ff4065626bcc0fd56d914
Tony Morris <tmorris@tmorris.net>
2012-04-29 18:49:09 +1000

    demo

--- demo/src/Math/Budget/Demo.hs
+++ demo/src/Math/Budget/Demo.hs
@@ -2,6 +2,7 @@ module Math.Budget.Demo where
 
 import Math.Budget
 import Data.Time
+import Network.URI
 
 bedrockTime ::
   Integer
@@ -22,4 +23,19 @@ mybudget =
         (knownInterval (fixedInterval everyWeek) 200)
         (bedrockTime 1966 11 20 (TimeOfDay 9 0 0))
         (bankDeposit "Fred Flintstone" "123456" "00118421")
+    , expensePayment
+        "Electricity"
+        undefined
+        (bedrockTime 1966 8 17 (TimeOfDay 12 0 0))
+        (internetMethod $ URI {
+          uriScheme = "http"
+        , uriAuthority = Just $ URIAuth {
+            uriUserInfo = ""
+          , uriRegName = "acmepower.com"
+          , uriPort = ""
+          }
+        , uriPath = "/paybill"
+        , uriQuery = "account=123"
+        , uriFragment = ""
+        })
     ]
